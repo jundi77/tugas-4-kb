a0e4b459ac6486453c0d189925425da0b6c06d21
Jundullah H. R <jundihanif77@gmail.com>
2021-04-05 11:27:35 +0700

    mencoba algo IDS

--- soal1b.py
+++ soal1b.py
@@ -10,10 +10,14 @@ class IDS: # iterative deepening search
         self.__node_found = None
         self.__found = False
         self.__depth = 0
+        self.__verbose = False
 
     def change_node(self, node):
         self.__node = node
         return self
+
+    def set_verbose(self, verbose):
+        self.__verbose = True
 
     def get_depth(self):
         return self.__depth
@@ -30,28 +34,37 @@ class IDS: # iterative deepening search
         self.__found  = False
         self.__node_found = None
         self.__target = target
+        if self.__verbose: print('Target -> ', self.__target.name)
+        if self.__verbose: print('*' * 10, ' Searching ', '*' * 10)
 
-        for depth in (0, max_depth + 1):
+        for depth in (0, max_depth):
             self.__depth = depth
+            self.__visited = {}
             node_result = self.dls_search_node(self.__node)
-            if self.__found:
+            if self.__found == True:
                 self.__node_found = node_result
                 return node_result
-
+            del self.__visited
+        if self.__verbose: print('*' * 10, ' End Searching ', '*' * 10)
         return False
 
     def dls_search_node(self, node, depth=0):
+        if self.__verbose: print("menuju ", node.name)
+        self.__visited[node] = True
         if self.__depth == depth:
+            if self.__verbose: print("depth mencapai ", self.__depth, " tidak melakukan pengecekan")
             return False
 
         # cek jika telah mencapai tujuan
         if node == self.__target:
+            if self.__verbose: print("Ditemukan ", node.name)
             self.__found = True
             return node
 
         depth = depth + 1
         result = False
         for child in node.child:
+            if self.__verbose: print("dari ", node.name, " mencoba ke ", child.name)
             result = self.dls_search_node(child, depth)
             if result != False:
                 break
@@ -59,4 +72,10 @@ class IDS: # iterative deepening search
         return result
 
 if __name__ == "__main__":
-    test = Node.init_simplified_romania()
+    nodes = Node.init_simplified_romania()
+    finder = IDS(nodes['Timisoara'])
+    finder.set_verbose(True)
+    finder.search(nodes['Bucharest'], 5)
+    print(finder.is_found())
+    # for node in nodes['Arad'].child:
+    # print(nodes['Arad'].child)
