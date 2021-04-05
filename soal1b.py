00c1cd8e735bf92e352eba9e0f4e0629d0f3730e
Jundullah H. R <jundihanif77@gmail.com>
2021-04-05 15:25:13 +0700

    solusi 1b ids

--- soal1b.py
+++ soal1b.py
@@ -7,7 +7,7 @@ class IDS: # iterative deepening search
     # ini sudah disesuaikan dengan kebutuhan 8 queens, yakni tak ada queen menyerang
     def __init__(self, node):
         self.__node = node
-        self.__node_found = None
+        self.__route = None
         self.__found = False
         self.__depth = 0
         self.__verbose = False
@@ -22,9 +22,9 @@ class IDS: # iterative deepening search
     def get_depth(self):
         return self.__depth
 
-    def get_node_found(self):
+    def get_route(self):
         if self.__found:
-            return self.__node_found
+            return self.__route
         return None
 
     def is_found(self):
@@ -32,12 +32,16 @@ class IDS: # iterative deepening search
 
     def search(self, target, max_depth):
         self.__found  = False
-        self.__node_found = None
         self.__target = target
-        if self.__verbose: print('Target -> ', self.__target.name)
+
+        if self.__route != None: del self.__route
+        self.__route = []
+
+        if self.__verbose: print('Target -> ', self.__target.name, "dari", self.__node.name)
         if self.__verbose: print('*' * 10, ' Searching ', '*' * 10)
 
-        for depth in (0, max_depth):
+        for depth in range(0, max_depth + 1):
+            if self.__verbose: print('#' * 6, 'search untuk depth', depth, "dari", max_depth, '#' * 6)
             self.__depth = depth
             self.__visited = {}
             node_result = self.dls_search_node(self.__node)
@@ -50,9 +54,9 @@ class IDS: # iterative deepening search
 
     def dls_search_node(self, node, depth=0):
         if self.__verbose: print("menuju ", node.name)
-        self.__visited[node] = True
-        if self.__depth == depth:
-            if self.__verbose: print("depth mencapai ", self.__depth, " tidak melakukan pengecekan")
+
+        if node in self.__visited:
+            if self.__verbose: print(node.name, " telah pernah dilewati")
             return False
 
         # cek jika telah mencapai tujuan
@@ -61,12 +65,18 @@ class IDS: # iterative deepening search
             self.__found = True
             return node
 
+        if self.__depth == depth:
+            if self.__verbose: print("depth mencapai ", self.__depth, " berhenti melakukan searching")
+            return False
+
         depth = depth + 1
         result = False
         for child in node.child:
             if self.__verbose: print("dari ", node.name, " mencoba ke ", child.name)
+            self.__visited[node] = True
             result = self.dls_search_node(child, depth)
             if result != False:
+                self.__route.insert(0, child)
                 break
 
         return result
@@ -74,8 +84,19 @@ class IDS: # iterative deepening search
 if __name__ == "__main__":
     nodes = Node.init_simplified_romania()
     finder = IDS(nodes['Timisoara'])
-    finder.set_verbose(True)
-    finder.search(nodes['Bucharest'], 5)
-    print(finder.is_found())
-    # for node in nodes['Arad'].child:
-    # print(nodes['Arad'].child)
+    # finder.set_verbose(True)
+    finder.search(nodes['Bucharest'], 4)
+    if finder.is_found():
+        out = "Rute ditemukan! "
+        prev = None
+        for r in finder.get_route():
+            out = out + r.name
+            if prev != None:
+                out = out + "(" + str(prev.child[r]['w']) + ")"
+            prev = r
+            out = out + " -> "
+        out = out + "sampai"
+        print(out)
+        exit()
+    print("Rute tidak ditemukan")
+    
